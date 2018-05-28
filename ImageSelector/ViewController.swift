c1a2db0386dcf3302b501cb521d2871a1d011b44
Leo Liu <leo.liu@aequilibrium.ca>
2018-05-28 16:52:09 -0700

    tableview with image names

--- ImageSelector/ViewController.swift
+++ ImageSelector/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var pictureNames = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //load some images
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("nssl"){
+                //load this picture
+                pictureNames.append(item)
+                
+            }
+        }
+        
+        print(pictureNames)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictureNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for:indexPath)
+        cell.textLabel?.text = pictureNames[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
